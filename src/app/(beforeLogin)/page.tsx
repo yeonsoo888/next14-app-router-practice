5e8408dba77e923fcd80bfce85721c951a5b1a07
yeonsoo888 <yeonsoo9846@gmail.com>
2024-03-08 14:17:32 +0900

    #add : "_component folder" 추가 ( url 영황 x )

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <div></div>
+    <div>
+      <Link href={'/i/flow/login'}>로그인화면으로</Link>
+    </div>
   );
 }
 
